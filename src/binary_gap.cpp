efb549c71453593162602a0f3723cc260084520d
Claus Guttesen <kometen@gmail.com>
2018-02-02 08:58:13 +0100

    Get max gap.

--- src/binary_gap.cpp
+++ src/binary_gap.cpp
@@ -8,28 +8,52 @@
 
 #include <bitset>
 #include <iostream>
-#include <limits>
-#include <string>
+//#include <limits>
 
 using namespace std;
 
 int solution(int N) {
-    int res = 0, x = 0;
-    bitset<16> tmp = N;
+    int x = 0, gap = 0, max = 0, current = 0, previous = 0;
+    bool start_counting = false;
+    bitset<32> tmp = N;
+    bitset<32> one = 1;
 
+    // Do-while loop while tmp is greater than one and count the gap when
+    // current and previous last bit differs.
     do {
+        current = (int)((tmp & one).to_ulong());
         x = (int)(tmp.to_ulong());
-        cout << "tmp: " << tmp << ", x: " << x << endl;
-        tmp>>=1;
-    } while (x > 0);
+//        cout << "tmp: " << tmp << ", x: " << x << ", current: " << current << endl;
 
-    return res;
+        // If the last bit is 1.
+        if ((tmp & one) == one) {
+//            cout << "and: " << (tmp & one) << endl;
+            gap = 0;
+        } else {
+            if (start_counting) {
+                ++gap;
+            }
+            // Get the largest gap.
+            if (gap > max) {
+                max = gap;
+            }
+        }
+
+        // Move one more to the right.
+        tmp >>= 1;
+        previous = (int)((tmp & one).to_ulong());
+        if (current != previous) {
+            start_counting = true;
+        }
+    } while (x > 1);
+
+    return max;
 }
 
 int main() {
     int gap = 0;
-    cout << "int max-size: " << numeric_limits<int>::max() << endl;
-    gap = solution(17);
+//    cout << "int max-size: " << numeric_limits<int>::max() << endl;
+    gap = solution(15);
     cout << "gap: " << gap << endl;
 	return 0;
 }
